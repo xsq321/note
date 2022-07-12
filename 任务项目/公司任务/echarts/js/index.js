96edb2e2176f8269663f54751676f2ae6d0bf41f
work <xiesq@plakeco.cn>
2022-07-12 17:24:34 +0800

    7.12 work更新

--- 任务项目/公司任务/echarts/js/index.js
+++ 任务项目/公司任务/echarts/js/index.js
@@ -33,6 +33,7 @@
         ],
         axisTick: {
           //坐标轴刻度相关设置
+          show: false, //去除小刻度线
           alignWithLabel: true, //保证刻度线与标签对齐，
         },
         //修改刻度标签相关样式
@@ -175,29 +176,316 @@
     ],
   };
   myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
 })();
 
-//饼图
+//折线图1
 (function () {
   var myChart = echarts.init(document.querySelector("#chart3"));
   var option = {
-    title: {
-      text: "饼图",
-      // subtext: "Fake Data",
-      left: "center",
+    color: ["#00f2f1", "#ed3f35"],
+    tooltip: {
+      trigger: "axis",
     },
+    legend: {
+      textStyle: {
+        //修改图例组件 文字颜色
+        color: "#4c9bfd",
+      },
+      right: "10%", //位置
+    },
+    grid: {
+      top: "20%",
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      show: true, //显示边框
+      borderColor: "#012f4a", //边框颜色
+      containLabel: true, //包含刻度文字在内
+    },
+    xAxis: {
+      type: "category",
+      boundaryGap: false, //去除轴间距（使折线图贴边）
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      axisTick: {
+        show: false, //去除刻度
+      },
+      axisLabel: {
+        color: "#4c9bfd",
+      },
+      axisLine: {
+        show: false,
+      },
+    },
+    yAxis: {
+      type: "value",
+      axisTick: {
+        show: false,
+      },
+      axisLabel: {
+        color: "#4c9bfd",
+      },
+      axisLine: {
+        show: false,
+      },
+      splitLine: {
+        lineStyle: {
+          color: "#012f4a",
+        },
+      },
+    },
+    series: [
+      {
+        name: "Email",
+        type: "line",
+        smooth: true, //平滑曲线显示
+        data: [120, 132, 101, 134, 90, 230, 210],
+      },
+      {
+        name: "Union Ads",
+        type: "line",
+        smooth: true,
+        data: [220, 182, 191, 234, 290, 330, 310],
+      },
+    ],
+  };
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+
+//折线图2
+(function () {
+  var myChart = echarts.init(document.querySelector("#chart4"));
+  var option = {
+    tooltip: {
+      trigger: "axis",
+    },
+    legend: {
+      top: "0%",
+      data: ["Email", "Union Ads"],
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12",
+      },
+    },
+    grid: {
+      left: "10",
+      top: "30",
+      right: "10",
+      bottom: "10",
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        data: [
+          "01",
+          "02",
+          "03",
+          "04",
+          "05",
+          "06",
+          "07",
+          "08",
+          "09",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "27",
+          "28",
+          "29",
+          "30",
+        ],
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12,
+          },
+        },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255,255,255,.2)",
+          },
+        },
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+        axisTick: {
+          show: false,
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12,
+          },
+        },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)",
+          },
+        },
+      },
+    ],
+    series: [
+      {
+        name: "Email",
+        type: "line",
+        smooth: true,
+        //修改线条样式
+        lineStyle: {
+          color: "#0184d5",
+          width: 2,
+        },
+        //设置填充样式（颜色）
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: "rgba(1,132,213,0.9)",
+              },
+              {
+                offset: 0.8,
+                color: "rgba(1,132,213,0.1)",
+              },
+            ],
+            false
+          ),
+          shadowColor: "rgba(0,0,0,.1)",
+        },
+        //设置拐点
+        symbol: "circle",
+        symbolSize: 8,
+        showSymbol: false, //设置成开始不显示，鼠标悬浮才显示
+        //设置拐点颜色及边框
+        itemStyle: {
+          color: "#0184d5",
+          borderColor: "rgba(211,220,107, .1)", //半透明边框
+          borderWidth: 12,
+        },
+        data: [
+          30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 30, 40, 30, 40, 30, 40, 30,
+          60, 20, 40, 30, 40, 30, 40, 30, 40, 20, 60, 50, 40,
+        ],
+      },
+      {
+        name: "Union Ads",
+        type: "line",
+        smooth: true,
+        lineStyle: {
+          color: "#00d887",
+          width: 2,
+        },
+        //设置填充样式（颜色）
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: "rgba(0,216,135,0.4)",
+              },
+              {
+                offset: 0.8,
+                color: "rgba(0,216,135,0.1)",
+              },
+            ],
+            false
+          ),
+          shadowColor: "rgba(0,0,0,.1)",
+        },
+        //设置拐点
+        symbol: "circle",
+        symbolSize: 5,
+        showSymbol: false, //设置成开始不显示，鼠标悬浮才显示
+        //设置拐点颜色及边框
+        itemStyle: {
+          color: "#00d887",
+          borderColor: "rgba(211,220,107, .1)", //半透明边框
+          borderWidth: 12,
+        },
+        data: [
+          80, 10, 20, 40, 30, 40, 80, 60, 20, 40, 90, 40, 20, 140, 30, 40, 130,
+          20, 20, 40, 80, 70, 30, 40, 30, 120, 20, 99, 50, 20,
+        ],
+      },
+    ],
+  };
+  myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+
+//饼图1
+(function () {
+  var myChart = echarts.init(document.querySelector("#chart5"));
+  var option = {
     tooltip: {
       trigger: "item",
     },
     legend: {
-      orient: "vertical",
-      left: "left",
+      bottom: "10%",
+      itemWidth: 15, //修改图表大小
+      itemHeight: 15,
+      //修改文字样式
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12",
+      },
     },
     series: [
       {
+        color: ["#065aab", "#066eab", "#0682ab", "#0696ab", "#06a0ab"], //也可写在 option 的下面，与 tooltip 同级
         name: "Access From",
         type: "pie",
-        radius: "50%",
+        center: ["50%", "40%"], //在容器中的位置，默认是水平垂直居中
+        radius: ["40%", "60%"], //修改内圆半径和外圆半径（饼图大小）
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: "center",
+        },
+        labelLine: {
+          show: false,
+        },
         data: [
           { value: 1048, name: "Search Engine" },
           { value: 735, name: "Direct" },
@@ -205,167 +493,75 @@
           { value: 484, name: "Union Ads" },
           { value: 300, name: "Video Ads" },
         ],
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: "rgba(0, 0, 0, 0.5)",
-          },
-        },
       },
     ],
   };
   myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
 })();
 
-//散点图
-(function () {
-  var myChart = echarts.init(document.querySelector("#chart4"));
-  var option = {
-    title: {
-      text: "散点图",
-    },
-    xAxis: {
-      scale: true,
-    },
-    yAxis: {
-      scale: true,
-    },
-    series: [
-      {
-        type: "effectScatter",
-        symbolSize: 20,
-        data: [
-          [172.7, 105.2],
-          [153.4, 42],
-        ],
-      },
-      {
-        type: "scatter",
-        // prettier-ignore
-        data: [[161.2, 51.6], [167.5, 59.0], [159.5, 49.2], [157.0, 63.0], [155.8, 53.6],
-                  [170.0, 59.0], [159.1, 47.6], [166.0, 69.8], [176.2, 66.8], [160.2, 75.2],
-                  [172.5, 55.2], [170.9, 54.2], [172.9, 62.5], [153.4, 42.0], [160.0, 50.0],
-                  [147.2, 49.8], [168.2, 49.2], [175.0, 73.2], [157.0, 47.8], [167.6, 68.8],
-                  [159.5, 50.6], [175.0, 82.5], [166.8, 57.2], [176.5, 87.8], [170.2, 72.8],
-                  [174.0, 54.5], [173.0, 59.8], [179.9, 67.3], [170.5, 67.8], [160.0, 47.0],
-                  [154.4, 46.2], [162.0, 55.0], [176.5, 83.0], [160.0, 54.4], [152.0, 45.8],
-                  [162.1, 53.6], [170.0, 73.2], [160.2, 52.1], [161.3, 67.9], [166.4, 56.6],
-                  [168.9, 62.3], [163.8, 58.5], [167.6, 54.5], [160.0, 50.2], [161.3, 60.3],
-                  [167.6, 58.3], [165.1, 56.2], [160.0, 50.2], [170.0, 72.9], [157.5, 59.8],
-                  [167.6, 61.0], [160.7, 69.1], [163.2, 55.9], [152.4, 46.5], [157.5, 54.3],
-                  [168.3, 54.8], [180.3, 60.7], [165.5, 60.0], [165.0, 62.0], [164.5, 60.3],
-                  [156.0, 52.7], [160.0, 74.3], [163.0, 62.0], [165.7, 73.1], [161.0, 80.0],
-                  [162.0, 54.7], [166.0, 53.2], [174.0, 75.7], [172.7, 61.1], [167.6, 55.7],
-                  [151.1, 48.7], [164.5, 52.3], [163.5, 50.0], [152.0, 59.3], [169.0, 62.5],
-                  [164.0, 55.7], [161.2, 54.8], [155.0, 45.9], [170.0, 70.6], [176.2, 67.2],
-                  [170.0, 69.4], [162.5, 58.2], [170.3, 64.8], [164.1, 71.6], [169.5, 52.8],
-                  [163.2, 59.8], [154.5, 49.0], [159.8, 50.0], [173.2, 69.2], [170.0, 55.9],
-                  [161.4, 63.4], [169.0, 58.2], [166.2, 58.6], [159.4, 45.7], [162.5, 52.2],
-                  [159.0, 48.6], [162.8, 57.8], [159.0, 55.6], [179.8, 66.8], [162.9, 59.4],
-                  [161.0, 53.6], [151.1, 73.2], [168.2, 53.4], [168.9, 69.0], [173.2, 58.4],
-                  [171.8, 56.2], [178.0, 70.6], [164.3, 59.8], [163.0, 72.0], [168.5, 65.2],
-                  [166.8, 56.6], [172.7, 105.2], [163.5, 51.8], [169.4, 63.4], [167.8, 59.0],
-                  [159.5, 47.6], [167.6, 63.0], [161.2, 55.2], [160.0, 45.0], [163.2, 54.0],
-                  [162.2, 50.2], [161.3, 60.2], [149.5, 44.8], [157.5, 58.8], [163.2, 56.4],
-                  [172.7, 62.0], [155.0, 49.2], [156.5, 67.2], [164.0, 53.8], [160.9, 54.4],
-                  [162.8, 58.0], [167.0, 59.8], [160.0, 54.8], [160.0, 43.2], [168.9, 60.5],
-                  [158.2, 46.4], [156.0, 64.4], [160.0, 48.8], [167.1, 62.2], [158.0, 55.5],
-                  [167.6, 57.8], [156.0, 54.6], [162.1, 59.2], [173.4, 52.7], [159.8, 53.2],
-                  [170.5, 64.5], [159.2, 51.8], [157.5, 56.0], [161.3, 63.6], [162.6, 63.2],
-                  [160.0, 59.5], [168.9, 56.8], [165.1, 64.1], [162.6, 50.0], [165.1, 72.3],
-                  [166.4, 55.0], [160.0, 55.9], [152.4, 60.4], [170.2, 69.1], [162.6, 84.5],
-                  [170.2, 55.9], [158.8, 55.5], [172.7, 69.5], [167.6, 76.4], [162.6, 61.4],
-                  [167.6, 65.9], [156.2, 58.6], [175.2, 66.8], [172.1, 56.6], [162.6, 58.6],
-                  [160.0, 55.9], [165.1, 59.1], [182.9, 81.8], [166.4, 70.7], [165.1, 56.8],
-                  [177.8, 60.0], [165.1, 58.2], [175.3, 72.7], [154.9, 54.1], [158.8, 49.1],
-                  [172.7, 75.9], [168.9, 55.0], [161.3, 57.3], [167.6, 55.0], [165.1, 65.5],
-                  [175.3, 65.5], [157.5, 48.6], [163.8, 58.6], [167.6, 63.6], [165.1, 55.2],
-                  [165.1, 62.7], [168.9, 56.6], [162.6, 53.9], [164.5, 63.2], [176.5, 73.6],
-                  [168.9, 62.0], [175.3, 63.6], [159.4, 53.2], [160.0, 53.4], [170.2, 55.0],
-                  [162.6, 70.5], [167.6, 54.5], [162.6, 54.5], [160.7, 55.9], [160.0, 59.0],
-                  [157.5, 63.6], [162.6, 54.5], [152.4, 47.3], [170.2, 67.7], [165.1, 80.9],
-                  [172.7, 70.5], [165.1, 60.9], [170.2, 63.6], [170.2, 54.5], [170.2, 59.1],
-                  [161.3, 70.5], [167.6, 52.7], [167.6, 62.7], [165.1, 86.3], [162.6, 66.4],
-                  [152.4, 67.3], [168.9, 63.0], [170.2, 73.6], [175.2, 62.3], [175.2, 57.7],
-                  [160.0, 55.4], [165.1, 104.1], [174.0, 55.5], [170.2, 77.3], [160.0, 80.5],
-                  [167.6, 64.5], [167.6, 72.3], [167.6, 61.4], [154.9, 58.2], [162.6, 81.8],
-                  [175.3, 63.6], [171.4, 53.4], [157.5, 54.5], [165.1, 53.6], [160.0, 60.0],
-                  [174.0, 73.6], [162.6, 61.4], [174.0, 55.5], [162.6, 63.6], [161.3, 60.9],
-                  [156.2, 60.0], [149.9, 46.8], [169.5, 57.3], [160.0, 64.1], [175.3, 63.6],
-                  [169.5, 67.3], [160.0, 75.5], [172.7, 68.2], [162.6, 61.4], [157.5, 76.8],
-                  [176.5, 71.8], [164.4, 55.5], [160.7, 48.6], [174.0, 66.4], [163.8, 67.3]
-              ],
-      },
-    ],
-  };
-  myChart.setOption(option);
-})();
-
-//雷达图
-(function () {
-  var myChart = echarts.init(document.querySelector("#chart5"));
-  var option = {
-    title: {
-      text: "雷达图",
-    },
-    legend: {
-      data: ["Allocated Budget", "Actual Spending"],
-    },
-    radar: {
-      // shape: 'circle',
-      indicator: [
-        { name: "Sales", max: 6500 },
-        { name: "Administration", max: 16000 },
-        { name: "Information Technology", max: 30000 },
-        { name: "Customer Support", max: 38000 },
-        { name: "Development", max: 52000 },
-        { name: "Marketing", max: 25000 },
-      ],
-    },
-    series: [
-      {
-        name: "Budget vs spending",
-        type: "radar",
-        data: [
-          {
-            value: [4200, 3000, 20000, 35000, 50000, 18000],
-            name: "Allocated Budget",
-          },
-          {
-            value: [5000, 14000, 28000, 26000, 42000, 21000],
-            name: "Actual Spending",
-          },
-        ],
-      },
-    ],
-  };
-  myChart.setOption(option);
-})();
-
-//数据集，对象数组的输入格式
+//饼图2
 (function () {
   var myChart = echarts.init(document.querySelector("#chart6"));
   var option = {
-    title: {
-      text: "数据集",
+    tooltip: {
+      trigger: "item",
     },
-    legend: {},
-    tooltip: {},
-    dataset: {
-      dimensions: ["product", "2015", "2016", "2017"],
-      source: [
-        { product: "Matcha Latte", 2015: 43.3, 2016: 85.8, 2017: 93.7 },
-        { product: "Milk Tea", 2015: 83.1, 2016: 73.4, 2017: 55.1 },
-        { product: "Cheese Cocoa", 2015: 86.4, 2016: 65.2, 2017: 82.5 },
-        { product: "Walnut Brownie", 2015: 72.4, 2016: 53.9, 2017: 39.1 },
-      ],
+    legend: {
+      bottom: "10%",
+      itemWidth: 15,
+      itemHeight: 15,
+      textStyle: {
+        color: "rgba(255,255,255,.5)",
+        fontSize: "12",
+      },
     },
-    xAxis: { type: "category" },
-    yAxis: {},
-    // Declare several bar series, each will be mapped
-    // to a column of dataset.source by default.
-    series: [{ type: "bar" }, { type: "bar" }, { type: "bar" }],
+    series: [
+      {
+        color: [
+          "#006cff",
+          "#60cda0",
+          "#ed8884",
+          "#ff9f7f",
+          "#0096ff",
+          "#9fe6b8",
+          "#32c5e9",
+          "#1d9dff",
+        ],
+        name: "Nightingale Chart",
+        type: "pie",
+        //每个模块旁边的文字提示
+        label: {
+          show: true,
+          textStyle: {
+            color: "rgba(255,255,255,0.5)",
+          },
+        },
+        //横线
+        labelLine: {
+          length: 6, //第一条线，链接图形的线条
+          length2: 8, //第二条线，链接文字的线条
+        },
+        radius: ["10%", "70%"],
+        center: ["50%", "40%"],
+        roseType: "radius", //显示模式为 半径 ；area 为面积模式
+        data: [
+          { value: 40, name: "rose 1" },
+          { value: 38, name: "rose 2" },
+          { value: 32, name: "rose 3" },
+          { value: 30, name: "rose 4" },
+          { value: 28, name: "rose 5" },
+          { value: 26, name: "rose 6" },
+          { value: 22, name: "rose 7" },
+          { value: 18, name: "rose 8" },
+        ],
+      },
+    ],
   };
   myChart.setOption(option);
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
 })();
